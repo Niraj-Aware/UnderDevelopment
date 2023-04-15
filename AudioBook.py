b713b23cb42e63b4181a76297a133bba95b7314f
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-04-16 00:44:33 +0530

    Update AudioBook.py

--- AudioBook.py
+++ AudioBook.py
@@ -1,45 +1,52 @@
 #!/usr/bin/env python
 
 import streamlit as st
-import time
 from pydub import AudioSegment
 from pydub.playback import play
 
+# Define the file path for the audiobook
+file_path = "C:\\Users\\niraj\\Downloads\\The_Red_Notebook.mp3"
 
-# Define the audio files and their names
-audio_files = {
-    "The Red Notebook": "C:\\Users\\niraj\\Downloads\\The_Red_NoteBook.mp3",
-    # Add more audiobook files here
-}
+def play_audio(audio_file):
+    # Load the audio file using Pydub
+    audio = AudioSegment.from_file(audio_file, format="mp3")
 
-
-
-# Define the audio player function
-def play_audio(file_path):
-    audio = AudioSegment.from_file(file_path, format="mp3")
+    # Play the audio file using Pydub
     play(audio)
 
-
-# Define the Streamlit app
 def main():
-    st.title("Audiobook Player")
-
     # Create a dropdown menu to select the audiobook to play
-    selected_audio = st.selectbox("Select an audiobook to play", list(audio_files.keys()))
+    audio_choice = st.sidebar.selectbox(
+        "Select Audiobook",
+        list(audio_files.keys())
+    )
 
-    # Load the selected audiobook
-    file_path = audio_files[selected_audio]
+    # Get the file path for the selected audiobook
+    audio_file = audio_files[audio_choice]
 
-    # Create play/pause, skip forward/backward buttons
-    if st.button('Play'):
-        play_audio(file_path)
-    elif st.button('Pause'):
-        play_audio(file_path).stop()
-    elif st.button('Skip Forward'):
-        play_audio(file_path + ' -ss 15')
-    elif st.button('Skip Backward'):
-        play_audio(file_path + ' -ss -15')
+    # Add buttons to play, pause, skip forward, and skip backward
+    if st.button("Play"):
+        play_audio(audio_file)
 
+    if st.button("Pause"):
+        play_audio(audio_file).stop()
+
+    if st.button("Skip Forward 15 Seconds"):
+        audio = AudioSegment.from_file(audio_file, format="mp3")
+        audio += 15 * 1000  # Add 15 seconds to the audio segment
+        play(audio)
+
+    if st.button("Skip Backward 15 Seconds"):
+        audio = AudioSegment.from_file(audio_file, format="mp3")
+        audio -= 15 * 1000  # Subtract 15 seconds from the audio segment
+        play(audio)
 
 if __name__ == "__main__":
+    # Define the available audiobooks
+    audio_files = {
+        "The Red Notebook": file_path,
+        # Add more audiobook files here
+    }
+
+    # Run the app
     main()
