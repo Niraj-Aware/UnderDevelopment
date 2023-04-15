ed43e2ee66c6e4bcc527266c90af261dc42aee70
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-04-16 00:46:32 +0530

    Update AudioBook.py

--- AudioBook.py
+++ AudioBook.py
@@ -2,51 +2,25 @@
 
 import streamlit as st
 from pydub import AudioSegment
-from pydub.playback import play
+import os
 
-# Define the file path for the audiobook
-file_path = "C:\\Users\\niraj\\Downloads\\The_Red_Notebook.mp3"
-
+# Function to play audio
 def play_audio(audio_file):
-    # Load the audio file using Pydub
-    audio = AudioSegment.from_file(audio_file, format="mp3")
+    audio = AudioSegment.from_file(audio_file)
+    audio.export("audio.wav", format="wav")
+    os.system("afplay audio.wav") # Replace with appropriate command for your OS
 
-    # Play the audio file using Pydub
-    play(audio)
-
+# Main function
 def main():
-    # Create a dropdown menu to select the audiobook to play
-    audio_choice = st.sidebar.selectbox(
-        "Select Audiobook",
-        list(audio_files.keys())
-    )
+    st.title("Audiobook Player")
 
-    # Get the file path for the selected audiobook
-    audio_file = audio_files[audio_choice]
+    # Upload audio file
+    audio_file = st.file_uploader("Upload an audio file in MP3 format", type=["mp3"])
 
-    # Add buttons to play, pause, skip forward, and skip backward
-    if st.button("Play"):
+    # Check if audio file is uploaded
+    if audio_file is not None:
+        # Play audio
         play_audio(audio_file)
 
-    if st.button("Pause"):
-        play_audio(audio_file).stop()
-
-    if st.button("Skip Forward 15 Seconds"):
-        audio = AudioSegment.from_file(audio_file, format="mp3")
-        audio += 15 * 1000  # Add 15 seconds to the audio segment
-        play(audio)
-
-    if st.button("Skip Backward 15 Seconds"):
-        audio = AudioSegment.from_file(audio_file, format="mp3")
-        audio -= 15 * 1000  # Subtract 15 seconds from the audio segment
-        play(audio)
-
 if __name__ == "__main__":
-    # Define the available audiobooks
-    audio_files = {
-        "The Red Notebook": file_path,
-        # Add more audiobook files here
-    }
-
-    # Run the app
     main()
